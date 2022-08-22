d8222f403f8959f76caf488937a0d098825d153a
Nikita <snitkonikita@gmail.com>
2022-08-22 22:47:53 +0400

    feat: add render cards for textbook

--- src/components/api.ts
+++ src/components/api.ts
@@ -1,59 +1,59 @@
 import { IUserData, IUserDataToken, IWordsData } from "./interfacec";
-import { DataStorage } from './dataStorage';
-import { AutorisationForm } from './autorisation/autorisation-form';
+import { DataStorage } from "./dataStorage";
+import { AutorisationForm } from "./autorisation/autorisation-form";
 
 export class API {
-    static url: string;
-    static header:{} = {'Content-Type': 'application/json','Accept': 'application/json'};
-    constructor(url: string){
-        API.url = url;
+  static url: string;
+  static header: {} = { "Content-Type": "application/json", Accept: "application/json" };
+  constructor(url: string) {
+    API.url = url;
+  }
+  static errorHandler(res: Response): Response {
+    if (!res.ok) {
+      if (res.status > 404) console.log(`Sorry, but there is ${res.status} error: ${res.statusText}`);
+      throw Error(res.statusText);
     }
-    static errorHandler(res: Response): Response {
-        if (!res.ok) {
-            if (res.status > 404) console.log(`Sorry, but there is ${res.status} error: ${res.statusText}`);
-            throw Error(res.statusText);
-        }
-        return res;
-}
-static async signinUsersFromServer(data:string): Promise<void> {
-    await fetch(`${this.url}/signin`, {method: 'POST', headers : API.header, body: data})
-        .then((response) => this.errorHandler(response))
-        .then((response) => response.json())
-        .then((data: IUserDataToken) => {
-            console.log(data);
-            DataStorage.userData = data;
-            AutorisationForm.closeModalWindow();
-        })
-        .catch((err) => {
-            console.log('Не удалось найти такого пользователя!!! Повторите попытку');
-        });
-}
-static async createUsersOnServer(data:string): Promise<void> {
-    await fetch(`${this.url}/users`, {method: 'POST', headers : API.header, body: data})
-        .then((response) => this.errorHandler(response))
-        .then((response) => response.json())
-        .then((data: IUserData) => {
-            console.log(data);
-        })
-        .catch((err) => console.log('Add User Error', err));
-}
-static async loadWordsFromServer(group:number, page:number){
-    await fetch(`${this.url}/words?group=${group}&page=${page}`, {method: 'GET', headers : API.header})
-    .then((response) => this.errorHandler(response))
-    .then((response) => response.json())
-    .then((data: IWordsData[]) => {
+    return res;
+  }
+  static async signinUsersFromServer(data: string): Promise<void> {
+    await fetch(`${this.url}/signin`, { method: "POST", headers: API.header, body: data })
+      .then((response) => this.errorHandler(response))
+      .then((response) => response.json())
+      .then((data: IUserDataToken) => {
         console.log(data);
-    })
-    .catch((err) => console.log('Add User Error', err));
-}
-static async loadWordToIdFromServer(id:string){
-        await fetch(`${this.url}/words/${id}`, {method: 'GET', headers : API.header})
-        .then((response) => this.errorHandler(response))
-        .then((response) => response.json())
-        .then((data: IWordsData) => {
-            DataStorage.wordId = data;
-            console.log(data);
-        })
-        .catch((err) => console.log('Add User Error', err));
-}
+        DataStorage.userData = data;
+        AutorisationForm.closeModalWindow();
+      })
+      .catch((err) => {
+        console.log("Не удалось найти такого пользователя!!! Повторите попытку");
+      });
+  }
+  static async createUsersOnServer(data: string): Promise<void> {
+    await fetch(`${this.url}/users`, { method: "POST", headers: API.header, body: data })
+      .then((response) => this.errorHandler(response))
+      .then((response) => response.json())
+      .then((data: IUserData) => {
+        console.log(data);
+      })
+      .catch((err) => console.log("Add User Error", err));
+  }
+  static async loadWordsFromServer(group: number, page: number): Promise<IWordsData[] | void> {
+    return await fetch(`${this.url}/words?group=${group}&page=${page}`, { method: "GET", headers: API.header })
+      .then((response) => this.errorHandler(response))
+      .then((response) => response.json())
+      .then((data: IWordsData[]) => {
+        return data;
+      })
+      .catch((err) => console.log("Add User Error", err));
+  }
+  static async loadWordToIdFromServer(id: string) {
+    await fetch(`${this.url}/words/${id}`, { method: "GET", headers: API.header })
+      .then((response) => this.errorHandler(response))
+      .then((response) => response.json())
+      .then((data: IWordsData) => {
+        DataStorage.wordId = data;
+        console.log(data);
+      })
+      .catch((err) => console.log("Add User Error", err));
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,56 @@
-import { AutorisationForm} from './components/autorisation/autorisation-form';
-import "./styles/css/app.css"
-import { API } from './components/api';
-import { DataStorage } from './components/dataStorage';
-import { StartPageListener } from './components/startPageListener';
+import { AutorisationForm } from "./components/autorisation/autorisation-form";
+import "./styles/css/app.css";
+import { API } from "./components/api";
+import { DataStorage } from "./components/dataStorage";
+import { StartPageListener } from "./components/startPageListener";
+import { renderCards } from "./components/textbook/textbook";
+import { IWordsData } from "./components/interfacec";
 
-const server: string = 'https://rs-lang-final-project.herokuapp.com';
+const server: string = "https://rs-lang-final-project.herokuapp.com";
 
 const autorisationForm: AutorisationForm = new AutorisationForm();
 const dataStorage: DataStorage = new DataStorage();
 const startPageListener: StartPageListener = new StartPageListener();
 const api: API = new API(server);
 autorisationForm.buttonAutorisationForm();
-API.loadWordsFromServer(0,0);
-API.loadWordToIdFromServer('5e9f5ee35eb9e72bc21af713');
 
+API.loadWordToIdFromServer("5e9f5ee35eb9e72bc21af713");
 
+const nav = document.querySelector(".nav");
+const btn = document.querySelector(".btn-word");
+const audio = document.getElementById('sound')
+
+btn?.addEventListener("click", (event: Event) => {
+    const btnWord = event.target as Element;
+    // if(btnWord.getAt){
+    //     console.log('sound')
+    // }
+});
+
+nav?.addEventListener("click", async (event) => {
+  const btn = event.target as HTMLElement;
+  const selectedNavItem = btn.getAttribute("data-nav");
+  //TODO: add
+  switch (selectedNavItem) {
+    case "home":
+      console.log(selectedNavItem);
+      break;
+
+    case "textbook":
+      const dataCards = await API.loadWordsFromServer(0, 0);
+      renderCards(dataCards as IWordsData[]);
+      break;
+
+    case "games":
+      console.log(selectedNavItem);
+      break;
+
+    case "statistics":
+      console.log(selectedNavItem);
+      break;
+
+    case "about":
+      console.log(selectedNavItem);
+      break;
+  }
+});
